be8696a92d2e5019a4864e11ee84fee841aef0b4
K.K Ranathunga <kashyaparanathunga2003@gmail.com>
2025-12-07 21:54:23 +0530

    Adding Director and Main Class

---
+++ 3/src/Director.java
@@ -0,0 +1,40 @@
+public class Director {
+    private String name;
+    private String surname;
+    private int numDirectedMovies;
+    private Date DOb;
+
+    public Director(String name, String surname){
+        this.name = name;
+        this.surname = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setNumDirectedMovies(int nMovies) {
+        this.numDirectedMovies = nMovies;
+    }
+
+    public void setDOb(Date DOb) {
+        this.DOb = DOb;
+    }
+
+    public int getNumDirectedMovies() {
+        return numDirectedMovies;
+    }
+
+    public Date getDOb() {
+        return DOb;
+    }
+
+    @Override
+    public String toString(){
+        return "Director [ name = "+name+", surname = "+surname, dob = 16/8/1954, movies directed = 23]
+    }
+}
